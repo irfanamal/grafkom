fecac8a86eca58c278ffce386d56809ecabb11d7
irfanamal <irfan.ihsanulamal@gmail.com>
2019-03-21 10:23:33 +0700

    Initial commit

---
+++ Task 1/pentagon.cpp
@@ -0,0 +1,28 @@
+#include <GL/glut.h>
+
+
+void polygon() {
+	glClear(GL_COLOR_BUFFER_BIT);
+	glBegin(GL_POLYGON);
+		glColor3f(0.0, 0.0, 0.0);
+		glVertex2d(0.0, 0.5);
+		glColor3f(0.0, 0.0, 0.0);
+		glVertex2d(0.35, 0.15);
+		glColor3f(0.0, 0.0, 0.0);
+		glVertex2d(0.25, -0.5);
+		glColor3f(0.0, 0.0, 0.0);
+		glVertex2d(-0.25, -0.5);
+		glColor3f(0.0, 0.0, 0.0);
+		glVertex2d(-0.35, 0.15);
+	glEnd();
+	glFlush();
+}
+
+int main(int argc, char **argv) {
+	glutInit(&argc, argv);
+	glutInitWindowSize(900, 600);
+	glutCreateWindow("Polygon");
+	glutDisplayFunc(polygon);
+	glutMainLoop();
+	return 0;
+}
