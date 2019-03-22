dee125867e2968906adc2cdd0772251c93f203c8
irfanamal <irfan.ihsanulamal@gmail.com>
2019-03-22 20:46:45 +0700

    warnain pentagonnya

--- Task1/pentagon.cpp
+++ Task1/pentagon.cpp
@@ -4,15 +4,15 @@
 void polygon() {
 	glClear(GL_COLOR_BUFFER_BIT);
 	glBegin(GL_POLYGON);
-		glColor3f(0.0, 0.0, 0.0);
+		glColor3f(1.0, 0.0, 0.0);
 		glVertex2d(0.0, 0.5);
-		glColor3f(0.0, 0.0, 0.0);
+		glColor3f(1.0, 0.0, 0.0);
 		glVertex2d(0.35, 0.15);
-		glColor3f(0.0, 0.0, 0.0);
+		glColor3f(1.0, 0.0, 0.0);
 		glVertex2d(0.25, -0.5);
-		glColor3f(0.0, 0.0, 0.0);
+		glColor3f(1.0, 0.0, 0.0);
 		glVertex2d(-0.25, -0.5);
-		glColor3f(0.0, 0.0, 0.0);
+		glColor3f(1.0, 0.0, 0.0);
 		glVertex2d(-0.35, 0.15);
 	glEnd();
 	glFlush();
